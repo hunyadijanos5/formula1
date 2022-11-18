795735f08aa8f7edc34da802ab14d27d9cf82eb4
hunyadijanos5 <hunyadi.janos5@gmail.com>
2022-11-18 13:20:20 +0100

    fill extra fields for drivers and sorting

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -1,5 +1,6 @@
 import { shuffle } from "../utils";
 import { Request, Response } from "express";
+import { iDriver } from "../interfaces";
 
 const router = require('express').Router();
 const drivers = require('../drivers.json')
@@ -9,7 +10,17 @@ router.get('/drivers', (req:Request, res: Response) => {
     res.header('Access-Control-Request-Headers', '*');
     res.header('Content-Type', 'application/json');
     
-    res.send(shuffle(drivers))
+    const places:number[] = []
+    for (let i = 0; i < drivers.length; i++) {
+        places.push(i + 1)
+    }
+    const randomPlaces = shuffle(places)
+    
+    res.send(drivers.map((d:iDriver, i:number) => ({
+        ...d,
+        imgUrl: `/images/${d.code.toLowerCase()}.png`,
+        place: randomPlaces[i]
+    })).sort((a:iDriver, b: iDriver) => (a?.place || 0) - (b?.place || 0)))
 });
 
 
